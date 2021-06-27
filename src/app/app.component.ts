c717418bc9ebbfa447e1e7c85781c94ebc25b46f
marceloferrer <marceloferrer@live.com.ar>
2021-06-27 12:31:08 -0300

    Test operators tap, map and take

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { error } from '@angular/compiler/src/util';
 import { Component, OnInit, VERSION } from '@angular/core';
 import { of, from } from 'rxjs';
+import { map, tap, take } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -15,11 +16,23 @@ export class AppComponent implements OnInit {
     of(2, 4, 6, 8).subscribe(console.log);
 
     //Suscribe number with from
-    from([20, 40, 60, 80]).subscribe(
-      item => console.log(`resulting item ... ${item}`),
-      error => console.log(`an error has ocurr ... ${error}`),
-      () => console.log(`Everything is complete`)
-    );
+    from([20, 15, 10, 5, 5, 0])
+      .pipe(
+        tap(item => console.log(`Original item ... ${item}`)),
+        map(item => item * 2),
+        map(item => item - 10),
+        map(item => {
+          if (item <= 0) {
+            throw new Error('Sub Zero');
+          }
+        }),
+        take(4)
+      )
+      .subscribe(
+        item => console.log(`resulting item ... ${item}`),
+        error => console.log(`an error has ocurr ... ${error}`),
+        () => console.log(`Everything is complete`)
+      );
 
     //Suscribe string with from
     from(['string1', 'string2', 'string3']).subscribe(
